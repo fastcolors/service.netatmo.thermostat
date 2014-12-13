386046d9fbd1a6126b6a42b0c0a4ed3fc5f78634
fastcolors <fastcolors@gmail.com>
2014-12-13 23:28:51 +0100

    split service and click

---
+++ default.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# encoding=utf-8
+
+import xbmc
+import xbmcgui
+import lnetatmo
+import sys
+
+
+print 'CIAOOOONEEEEEEE'
--- service.py
+++ service.py
@@ -1,30 +1,19 @@
-#!/usr/bin/python3
-# encoding=utf-8
 
 import xbmc
 import xbmcgui
 import lnetatmo
 
+xbmc.log("### run_backend started")
+
+authorization = lnetatmo.ClientAuth()
+monitor = xbmc.Monitor()
+
 WINDOW = xbmcgui.Window(10000)
 
-monitor = xbmc.Monitor()
-authorization = lnetatmo.ClientAuth()
-devList = lnetatmo.DeviceList(authorization)
-
-relaycommand = unicode(devList.thermrelaycmd)
-temp = str(devList.temperature)
-settemp = str(devList.setpoint_temp)
-setmode = str(devList.setpoint_mode)
-
-WINDOW.setProperty('HomeTemperature', temp)
-WINDOW.setProperty('HomeSetTemperature', settemp)
-WINDOW.setProperty('HomeSetMode', setmode)
-WINDOW.setProperty('RelayCommand', relaycommand)
-
-while not monitor.waitForAbort(lnetatmo._REFRESH):
-
-    print 'REFRESHED NETATMO REFRESHED NETATMO REFRESHED NETATMO REFRESHED NETATMO REFRESHED NETATMO'
-
+for prop in ("HomeTemperature", "HomeSetTemperature", "HomeSetMode", "RelayCommand"):
+    WINDOW.clearProperty(prop)
+    
+def retrieve():
     devList = lnetatmo.DeviceList(authorization)
 
     relaycommand = unicode(devList.thermrelaycmd)
@@ -36,3 +25,9 @@ while not monitor.waitForAbort(lnetatmo._REFRESH):
     WINDOW.setProperty('HomeSetTemperature', settemp)
     WINDOW.setProperty('HomeSetMode', setmode)
     WINDOW.setProperty('RelayCommand', relaycommand)
+
+retrieve()
+
+while not monitor.waitForAbort(30):
+
+    retrieve()
